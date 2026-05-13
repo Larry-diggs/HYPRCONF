4a7febcf7d4bdbea03c444a8ca2ebcc08a6e2d90
Larry-diggs <nstrohl1998@gmail.com>
2026-05-12 22:20:07 -0400

    Add environment variable settings in envs.lua
    Added environment variable configurations for cursor themes and display settings.

--- envs.lua
+++ envs.lua
@@ -1,1 +1,12 @@
-
+-- ==================
+-- ENVIRONMENT VARIABLES
+-- ==================
+hl.env("HYPRCURSOR_THEME", "rose-pine-hyprcursor")
+hl.env("HYPRCURSOR_SIZE",  "19")
+hl.env("XCURSOR_THEME",    "BreezeX-Black")
+hl.env("XCURSOR_SIZE",     "19")
+hl.env("GDK_BACKEND",      "wayland")
+hl.env("XCURSOR_PATH",     "/home/USERNAME/.local/share/icons:/usr/local/share/icons:/usr/share/icons")
+hl.env("SDL_VIDEO_WAYLAND_SCALE_TO_DISPLAY", "1")
+hl.env("QT_QPA_PLATFORMTHEME",      "qt6ct")
+hl.env("QT_QPA_PLATFORMTHEME_QT6",  "qt6ct")
